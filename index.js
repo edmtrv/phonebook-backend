2aea4d2210ff345b7dd1d3879d2d6100d27bd8fd
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-18 18:01:48 +0200

    Phonebook backend step 8 done

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+
+
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('data', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(
+  ':method :url :status :res[content-length] - :response-time ms :data'
+));
 
 let persons = [
   {
