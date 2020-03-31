209e349543c4a2d6434375f20f9a2b757a745477
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-31 14:41:10 +0300

    Eslint and prettier setup

--- index.js
+++ index.js
@@ -11,56 +11,58 @@ app.use(cors());
 app.use(express.static('build'));
 morgan.token('data', (req, res) => JSON.stringify(req.body));
 
-app.use(morgan(
-  ':method :url :status :res[content-length] - :response-time ms :data'
-));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+);
 
 app.get('/info', (req, res) => {
-  Person.find({}).then(persons => {
-    const html = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  Person.find({}).then((persons) => {
+    const html = `<p>Phonebook has info for ${
+      persons.length
+    } people</p><p>${new Date()}</p>`;
 
     res.send(html);
   });
 });
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons.map(p => p.toJSON()));
+  Person.find({}).then((persons) => {
+    res.json(persons.map((p) => p.toJSON()));
   });
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
         res.json(person);
       } else {
         res.status(404).end();
       }
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
+    .then((result) => {
       res.status(204).end();
     })
-    .catch(err => next(err))
+    .catch((err) => next(err));
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
   const body = req.body;
 
   const person = {
-    number: body.number
+    number: body.number,
   };
 
   Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then(updatedPerson => {
-      res.json(updatedPerson.toJSON())
+    .then((updatedPerson) => {
+      res.json(updatedPerson.toJSON());
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
 app.post('/api/persons', (req, res, next) => {
@@ -68,27 +70,28 @@ app.post('/api/persons', (req, res, next) => {
 
   const person = new Person({
     name: body.name,
-    number: body.number
+    number: body.number,
   });
 
-  person.save()
-    .then(result => {
+  person
+    .save()
+    .then((result) => {
       res.json(result);
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
 const errorHandler = (err, req, res, next) => {
   console.log(err.message);
 
   if (err.name === 'CastError' && err.kind === 'ObjectId') {
-    return res.status(400).send({ error: 'Malformatted id'});
+    return res.status(400).send({ error: 'Malformatted id' });
   } else if (err.name === 'ValidationError') {
-    return res.status(400).send({ error: err});
+    return res.status(400).send({ error: err });
   }
 
   next(err);
-}
+};
 
 app.use(errorHandler);
 
